dac9ebf3989552a2a5aab9660e07188432e2ca13
Avery Allen <averyallen71@gmail.com>
2024-12-12 19:49:42 -0500

    Change formatting of lsp config

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -30,9 +30,29 @@ return {
     })
 
     -- Global LSP keymaps
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true, silent = true, desc = "Go to Definition" })
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, { noremap = true, silent = true, desc = "Hover Documentation" })
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, { noremap = true, silent = true, desc = "References" })
-    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { noremap = true, silent = true, desc = "Code Action" })
+    vim.keymap.set(
+      "n",
+      "gd",
+      vim.lsp.buf.definition,
+      { noremap = true, silent = true, desc = "Go to Definition" }
+    )
+    vim.keymap.set(
+      "n",
+      "K",
+      vim.lsp.buf.hover,
+      { noremap = true, silent = true, desc = "Hover Documentation" }
+    )
+    vim.keymap.set(
+      "n",
+      "gr",
+      vim.lsp.buf.references,
+      { noremap = true, silent = true, desc = "References" }
+    )
+    vim.keymap.set(
+      "n",
+      "<leader>ca",
+      vim.lsp.buf.code_action,
+      { noremap = true, silent = true, desc = "Code Action" }
+    )
   end,
 }
