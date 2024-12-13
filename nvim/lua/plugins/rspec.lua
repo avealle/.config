3cfdf057932605605d51fd65e616e83b3b53c61a
Avery Allen <averyallen71@gmail.com>
2024-12-12 19:47:07 -0500

    Remap rspec commands

--- nvim/lua/plugins/rspec.lua
+++ nvim/lua/plugins/rspec.lua
@@ -2,10 +2,10 @@ return {
   {
     "thoughtbot/vim-rspec",
     config = function()
-      vim.keymap.set("n", "<Leader>t", ":call RunCurrentSpecFile()<CR>", { noremap = true, silent = true })
-      vim.keymap.set("n", "<Leader>s", ":call RunNearestSpec()<CR>", { noremap = true, silent = true })
-      vim.keymap.set("n", "<Leader>l", ":call RunLastSpec()<CR>", { noremap = true, silent = true })
-      vim.keymap.set("n", "<Leader>a", ":call RunAllSpecs()<CR>", { noremap = true, silent = true })
+      vim.keymap.set("n", "<Leader>tf", ":call RunCurrentSpecFile()<CR>", { noremap = true, silent = true })
+      vim.keymap.set("n", "<Leader>tn", ":call RunNearestSpec()<CR>", { noremap = true, silent = true })
+      vim.keymap.set("n", "<Leader>tf", ":call RunLastSpec()<CR>", { noremap = true, silent = true })
+      vim.keymap.set("n", "<Leader>ta", ":call RunAllSpecs()<CR>", { noremap = true, silent = true })
 
       vim.g.rspec_command = "!bundle exec rspec {spec}"
     end,
