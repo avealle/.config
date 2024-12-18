399ad0c0536eb20967e196a8cc6d1352f74c04d6
Avery Allen <avery.a@buoysoftware.com>
2024-12-18 12:28:55 -0500

    Fix repeated spec keymap

--- nvim/lua/plugins/rspec.lua
+++ nvim/lua/plugins/rspec.lua
@@ -4,7 +4,7 @@ return {
     config = function()
       vim.keymap.set("n", "<Leader>tf", ":call RunCurrentSpecFile()<CR>", { noremap = true, silent = true })
       vim.keymap.set("n", "<Leader>tn", ":call RunNearestSpec()<CR>", { noremap = true, silent = true })
-      vim.keymap.set("n", "<Leader>tf", ":call RunLastSpec()<CR>", { noremap = true, silent = true })
+      vim.keymap.set("n", "<Leader>tl", ":call RunLastSpec()<CR>", { noremap = true, silent = true })
       vim.keymap.set("n", "<Leader>ta", ":call RunAllSpecs()<CR>", { noremap = true, silent = true })
 
       vim.g.rspec_command = "!bundle exec rspec {spec}"
